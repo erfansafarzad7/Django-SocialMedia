a046e961892e883ff5e523e5fc74ccad171f18a9
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-09-12 12:31:34 +0430

    forms-validation-update

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from .forms import UserRegisForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.db import IntegrityError
+
 
 
 class RegisterView(View):
@@ -17,24 +19,13 @@ class RegisterView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'], cd['email'], cd['password'])
-            messages.success(request, 'Registered Successfully', 'success')
-            return redirect('home:home')
+
+            try:
+                User.objects.create_user(cd['username'], cd['email'], cd['password'])
+                messages.success(request, 'Registered Successfully', 'success')
+                return redirect('home:home')
+            except IntegrityError as e:
+                messages.warning(request, 'username already exist', 'warning')
+                return render(request, self.temp_name, {'form': form})
+
         return render(request, self.temp_name, {'form': form})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
