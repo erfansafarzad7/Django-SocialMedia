bc72ac75063ade7229df7636e0e6c8531425bd4b
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-09-13 08:30:39 +0430

    admin-fields

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'created', 'updated')
+    search_fields = ('slug', 'body')
+    list_filter = ('created', 'updated')
+    prepopulated_fields = {'slug': ('body',)}
+    row_id_fields = ('user',)
+
+# admin.site.register(Post, PostAdmin)
