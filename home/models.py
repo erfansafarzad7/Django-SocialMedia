f48fba790bc108de0d72de5b0db7e3fd7b5565fb
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-09-13 09:58:25 +0430

    urls-update

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -12,4 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.user} -- {self.slug}"
 
-
+    def get_absolut_url(self):
+        return reverse('home:post_detail', args=(self.id, self.slug))
