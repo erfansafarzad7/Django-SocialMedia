cb9b8906b22bab2869919cb238ffa4a07555401f
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-09-13 18:07:05 +0430

    update-post-performance

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -33,9 +34,10 @@ class PostDeleteView(LoginRequiredMixin, View):
                 messages.success(request, 'Post Successfully Deleted', 'success')
             else:
                 messages.error(request, 'You Can Not Delete This Message')
-            user = User.objects.get(pk=user_id)
-            posts = Post.objects.filter(user=user)
-            return render(request, 'accounts/profile.html', {'user': user, 'posts': posts})
+            # user = User.objects.get(pk=user_id)
+            # posts = Post.objects.filter(user=user)
+            # return render(request, 'accounts/profile.html', {'user': user, 'posts': posts})
+            return redirect('accounts:user_profile', post.user.id)
         except ObjectDoesNotExist:
             messages.warning(request, 'Page Not Found', 'warning')
             return redirect('home:home')
@@ -44,19 +46,29 @@ class PostDeleteView(LoginRequiredMixin, View):
 class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostUpdateForm
 
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if post.user.id != request.user.id:
             messages.error(request, 'You Cant Edit This Message', 'danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'home/update.html', {'form': form})
 
-    def post(self, request, post_id):
-        pass
-
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:20])
+            new_post.save()
+            messages.success(request, 'Your Post Updated', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
 
